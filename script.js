0f59aab266434e62ae43f2f753966172dd38afc8
Parthipan G <amail2parthivijay@gmail.com>
2024-05-19 18:48:06 +0530

    HTML DOM files added

---
+++ script.js
@@ -0,0 +1,10 @@
+function foo(){
+
+    var firstname = document.getElementById('firstname').value
+    var middlename = document.getElementById('middlename').value
+    var lastname = document.getElementById('lastname').value
+    var email = document.getElementById('email').value
+    var phonenumber = document.getElementById('phone').value
+
+    console.log(`First Name: ${firstname}\nMiddle Name: ${middlename}\nLast Name: ${lastname}\nEmail: ${email}\nPhone Number: ${phonenumber}`)
+}
